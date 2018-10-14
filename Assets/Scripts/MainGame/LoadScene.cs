5268f03deecbdd254d13fe2dffdb36586b538e30
IkadaiYuya <yuuyayou@gmail.com>
2018-10-14 21:05:55 +0900

    2018/10/14
    サウンド等の追加

---
+++ Assets/Scripts/MainGame/LoadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(PlayerIsDead())
+        {
+            SceneManager.LoadScene("Results");
+        }
+	}
+
+    private bool PlayerIsDead()
+    {
+        GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log(player.Length);
+        if(player.Length <= 0 )
+        {
+            return true;
+        }
+        return false;
+    }
+}
